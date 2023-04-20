2ca66e4513c79b72d93f00a48d0a8903c9d09a67
pjastr <24464854+pjastr@users.noreply.github.com>
2023-04-20 17:15:54 +0200

    init

---
+++ DebugAH/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo(int*a,int*b)
+{
+    return *a-*b;
+}
+
+int main()
+{
+    int tab[] = {3,3,0,0,3,3,-4,-4,0,1};
+    int *wsk=tab-1;
+    int b = *(wsk+=4); //b=
+    int c = b+3; // b=  , c=
+    int d = foo(&b,&c); // b=  , c=  , d=
+    int e = (wsk+=-1)[4]; // b=  , c=  , d=  , e=
+    e = (d *= 2) + (c += 2); // b=  , c=  , d=  , e=
+    c = d + (b+=4); // b=  , c=  , d=  , e=
+    e= (--c)-(d++);  // b=  , c=  , d=  , e=
+    b = *wsk + e; // b=  , c=  , d=  , e=
+    return 0;
+}
---
+++ DebugAL/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo(int*a,int*b)
+{
+    return *a-2**b;
+}
+
+int main()
+{
+    int tab[] = {1,-1,2,2,0,-3,-4,-4,1,1};
+    int *wsk=tab-1;
+    int b = *(wsk+=4); //b=
+    int c = b+3; // b=  , c=
+    int d = foo(&b,&c); // b=  , c=  , d=
+    int e = (wsk+=-1)[4]; // b=  , c=  , d=  , e=
+    c = d + (b+=4); // b=  , c=  , d=  , e=
+    e= (--c)-(d++);  // b=  , c=  , d=  , e=
+    b = *wsk + e; // b=  , c=  , d=  , e=
+    e = (d -= 2) + (c += 2); // b=  , c=  , d=  , e=
+    return 0;
+}
